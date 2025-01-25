754f7eaf0909a81b255f05d55cf8e854f2c03551
Александр Архипов <liney13@mail.ru>
2025-01-25 17:55:27 +0800

    arr & arr2D DONE

--- Arrays/index.php
+++ Arrays/index.php
@@ -9,6 +9,8 @@ $array2=[34,55,89,144]; //объ€вление через не€вный вызов конструктора
 $array2[] = 233; //добавление в конец массива нового элемента
 unset($array2[4]);
 
+$arr_hw=[];
+
 //$distance_to_sun['Mercury'] = 46000000;
 //$distance_to_sun['Venus'] = 108000000;
 //$distance_to_sun['Earth'] = 149000000;
@@ -30,6 +32,7 @@ $array2D=
 	[34,55,89]
 ];
 
+$arr_hw2D=[];
 function print_array($array)
 {
 	echo '<pre>';
@@ -47,5 +50,82 @@ function print_array_pre($array)
 	echo '</pre>';
 }
 
+function max_in_arr($arr)
+{
+	rsort($arr);
+	return $arr[0];
+}
+function min_in_arr($arr)
+{
+	sort($arr);
+	return $arr[0];
+}
+function sum_arr($arr)
+{
+	$sum=0;
+	foreach($arr as $iterator)
+		{
+			$sum+=$iterator;
+		}
+	return $sum;
+}
+function avg_arr($arr)
+{
+	return sum_arr($arr)/count($arr);
+}
+
+function print_array2D($arr)
+{
+	foreach($arr as $iterator)
+		{
+			foreach($iterator as $iterator)
+			{
+				echo"$iterator\t";
+			}
+			echo'<br>';
+		}
+}
+function max_in_arr2D($arr)
+{
+	$max=$arr[0][0];
+	foreach($arr as $iterator)
+		{
+			foreach($iterator as $iterator)
+			{
+				if ($iterator>$max) $max=$iterator;
+			}
+		}
+	return $max;
+}
+function min_in_arr2D($arr)
+{
+	$min=$arr[0][0];
+	foreach($arr as $iterator)
+		{
+			foreach($iterator as $iterator)
+			{
+				if ($iterator<$min) $min=$iterator;
+			}
+		}
+	return $min;
+}
+function sum_arr2D($arr)
+{
+	$sum=0;
+	foreach($arr as $iterator)
+	{
+		$sum+=sum_arr($iterator);
+	}
+	return $sum;
+}
+function avg_arr2D($arr)
+{
+	$count=0;
+	foreach($arr as $iterator)
+	{
+		$count+=count($iterator);
+	}
+	return sum_arr2D($arr)/$count;
+}
 
 require 'index.view.php';
