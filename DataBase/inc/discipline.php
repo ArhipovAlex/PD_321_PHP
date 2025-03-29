0197b416f5a857b8cd5c8ffb43ae50e6555b6702
Александр Архипов <liney13@mail.ru>
2025-03-29 19:07:40 +0800

    Refresh Discipline Page DONE

--- DataBase/inc/discipline.php
+++ DataBase/inc/discipline.php
@@ -1,35 +1,58 @@
-<?php
-	require_once __DIR__.'/header.html';
-	require_once __DIR__.'/connection.php';
-	$query = "SELECT * FROM disciplines WHERE discipline_id={$_REQUEST['id']}";
-	$result = sqlsrv_query($connection, $query);
-	if($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
-	{
-		echo "<h1>{$row['discipline_name']}</h1>";
-		echo "Количество занятий: {$row['number_of_lessons']}";
-		echo "<h3>Преподаватели:</h3>";
-		echo '<div class="table-with-combobox" style="display:flex;justify-content:space-between;">';
-		
-			echo '<div>';
-				echo '<table>';
-					echo '<thead>';
-					echo '</thead>';
-					echo '<tbody id="table-teachers">';
-						require_once __DIR__.'/get_teachers_for_discipline.php';
-					echo '</tbody>';
-				echo '</table>';
-			echo '</div>';
-			echo '<div style="margin-right:25px;text-align:right;">';
-				echo '<select>';
-				echo '<option value="0">';
-				echo '-- Добавить преподавателя --';
-				echo '</option>';
-				require_once __DIR__ . '/get_teachers_to_combo.php';
-				echo '</select>';
-				echo '<br>';
-				echo '<input type="button" value="Добавить" style="margin-top:10px;">';
-			echo '</div>';
-		echo '</div>';
-	}
-	require_once __DIR__.'/footer.html';
+﻿<?php
+    require_once __DIR__ . '/get_discipline.php';
 ?>
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+	<link rel="stylesheet" href="../css/style.css">
+    <title><?=$row['discipline_name']?></title>
+</head>
+<body>
+    <h1><?= $row['discipline_name']?></h1>
+	<div>Номер дисциплины: <span id="discipline-id"><?= $row['discipline_id'];?></span></div>
+	<div>Количество занятий: <?= $row['number_of_lessons'];?></div>
+    	<h3>Преподаватели:</h3>
+		<div class="table-with-combobox" style="display:flex;justify-content:space-between;">		
+			<div>
+				<table id="teachers">
+					<thead>
+					</thead>
+					<tbody id="table-teachers">
+						<?php require_once __DIR__.'/get_teachers_for_discipline.php';?>
+					</tbody>
+				</table>
+			</div>
+			<div style="margin-right:25px;text-align:right;">
+				<select id="selected-teacher">
+				<option value="0">
+				-- Добавить преподавателя --
+				</option>
+				<?php require_once __DIR__ . '/get_teachers_to_combo.php'; ?>
+				</select>
+				<br>
+				<input type="button" value="Добавить" style="margin-top:10px;" onclick="addTeacherForDiscipline()">
+			</div>
+		</div>
+	<div id="debug"></div>
+	<div id="responce"></div>
+    <script>
+		function addTeacherForDiscipline()
+		{
+			let teacher_id = document.getElementById("selected-teacher").value;
+			let discipline_id = document.getElementById("discipline-id").innerText;
+			alert(`ID:${discipline_id}; Teacher:${teacher_id}`);
+
+			let request = new XMLHttpRequest();
+			request.onreadystatechange = function()
+			{
+				document.getElementById("responce").innerText = this.responseText;
+			}
+			request.open("GET",`set_teacher_for_discipline.php?teacher_id=${teacher_id}&discipline_id=${discipline_id}`,true);
+			request.send();
+			window.location.reload(true);
+		}
+    </script>
+</body>
+</html>
